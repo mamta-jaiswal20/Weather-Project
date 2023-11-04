3011e62aee9fe2d2181a45e8a27af8a0f35dbbf0
mamta-jaiswal20 <mj20042001@gmail.com>
2023-11-04 23:43:51 +0530

    Use of Json stringify

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const https = require("https");
+
 const app = express();
 
 app.get("/", function (req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&APPID=717cd8e52c48e2b5557a2d93f751f6df&units=metric"
+
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const object = {
+                name: "Mamta",
+                favFood: "Ramen"
+            }
+            console.log(JSON.stringify(object));
+        })
+    });
     res.send("Server is up and running")
 });
 
